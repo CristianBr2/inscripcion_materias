ba4659706f2af07389cefe76d19a78843a8c3d13
emalgss <emajunior234lagos@gmail.com>
2025-10-25 22:39:54 -0300

    Detallando Servicios.jsx

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -15,7 +15,7 @@ function Servicios() {
           <button onClick={()=>navigate("/solicitarmesa")}>Solicitar mesa</button>
           <button onClick={()=>navigate("/Horario")}>Hs profesores</button>
           <button onClick={() => navigate("/problema")}>Tengo un problema</button>
-          <button onClick={()=>navigate("/Inscripcion")}>Inscribirme a materias</button>
+          <button onClick={()=>navigate("/InscripcionMateria")}>Inscribirme a materias</button>
         </div>
       </div>
     </div>
