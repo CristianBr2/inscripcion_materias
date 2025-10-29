a4cc1302a9dd3a6df4bdc69706214e5bf6711034
CristianBr2 <antinir.cristian.23@gmail.com>
2025-10-29 18:23:59 -0300

    Verificación del rol

--- src/utils/getRolUsuario.js
+++ src/utils/getRolUsuario.js
@@ -4,4 +4,9 @@ import { db } from "../firebaseConfig";
 export const getRolUsuario=async(uid)=>{
     const docRef= doc(db,"Usuario_nuevo", uid);
     const docSnap=await getDoc(docRef);
+    if (docSnap.exists()) {
+    return docSnap.data().rol || "usuario";
+    } else {
+        return "usuario";
+    }
 }
