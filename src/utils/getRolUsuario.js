ce6435f426c0bbac853b545558a9de09af571479
CristianBr2 <antinir.cristian.23@gmail.com>
2025-10-29 18:16:57 -0300

    Función para roles + colección + consulta

--- src/utils/getRolUsuario.js
+++ src/utils/getRolUsuario.js
@@ -1,2 +1,7 @@
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "../firebaseConfig";
+
+export const getRolUsuario=async(uid)=>{
+    const docRef= doc(db,"Usuario_nuevo", uid);
+    const docSnap=await getDoc(docRef);
+}
