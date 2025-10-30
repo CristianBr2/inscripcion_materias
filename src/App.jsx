897a82f687dccafb6eec4968c0f243b86b824a84
CristianBr2 <antinir.cristian.23@gmail.com>
2025-10-29 22:49:07 -0300

    Limpiando archivo + Correccion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -22,39 +22,32 @@ import { UserProvider } from './context/UserContext.js';
 function App() {
   return (
     <UserProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        {/* <Route path="/registro" element={<Registro />} /> */}
-        {/* <Route path="/registro-exitoso" element={<RegistroExitoso />} /> */}
-        {/* <Route path="/mesa-examen" element={<MesaExamen/>} /> */}
-         {/* <Route path="/alumno" element={<Alumno/>} /> */}
-        {/* { <Route path="/registro" element={<Registro />} /> } redirección libre */}
-        {/* <Route path="/registro-exitoso" element={<RegistroExitoso />} />
-        <Route path="/servicios" element={<Servicios />} />
-        <Route path="/mesa-examen" element={<UseMesaExamen />} />
-        <Route path="/alta-mesa" element={<AltaMesaExamen />} />
-        <Route path="/producto/:id" element={<Detalle />} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* redireccion bloqueada */}
-        <Route path="/alumno" element={<RutaPrivada><Alumno/></RutaPrivada>} />
-        <Route path="/servicios" element={<RutaPrivada><Servicios /></RutaPrivada>}/> 
-        <Route path="/mesa-examen" element={<RutaPrivada><UseMesaExamen /></RutaPrivada>} />
-        <Route path="/alta-mesa" element={<RutaPrivada><AltaMesaExamen /></RutaPrivada>}/>
-        <Route path="/producto/:id" element={<RutaPrivada><Detalle /></RutaPrivada>} />
-        <Route path="/registro" element={<RutaPrivada><Registro /></RutaPrivada>} />
-        <Route path="/registro-exitoso" element={<RutaPrivada><RegistroExitoso /></RutaPrivada>}/>
-        <Route path="/programa" element={<RutaPrivada><Programa /></RutaPrivada>} />
-        <Route path="/horario" element={<RutaPrivada><Horario /></RutaPrivada>} />
-        <Route path="/solicitarmesa" element={<RutaPrivada><SolicitarMesa /></RutaPrivada>} />
-        <Route path="/cargacorrecta" element={<RutaPrivada><CargaCorrecta /></RutaPrivada>} />
-        <Route path="/InscripcionMateria" element={<RutaPrivada><InscripcionMateria /></RutaPrivada>} />
-        <Route path="/InscriptoCorrectamente" element={<RutaPrivada><InscriptoCorrectamente /></RutaPrivada>} />
-        <Route path="/Contacto" element={<RutaPrivada><Contacto /></RutaPrivada>} />  
-        <Route path="/Gracias" element={<RutaPrivada><Gracias /></RutaPrivada>} />  
-      </Routes>
-    </BrowserRouter>
+          {/* rutas usuario normal (y admin tbm) */}
+          <Route path="/alumno" element={<RutaPrivada requiredRole="usuario"><Alumno/></RutaPrivada>} />
+          <Route path="/servicios" element={<RutaPrivada requiredRole="usuario"><Servicios /></RutaPrivada>} />
+          <Route path="/registro" element={<RutaPrivada requiredRole="usuario"><Registro /></RutaPrivada>} />
+          <Route path="/registro-exitoso" element={<RutaPrivada requiredRole="usuario"><RegistroExitoso /></RutaPrivada>} />
+          <Route path="/programa" element={<RutaPrivada requiredRole="usuario"><Programa /></RutaPrivada>} />
+          <Route path="/horario" element={<RutaPrivada requiredRole="usuario"><Horario /></RutaPrivada>} />
+          <Route path="/solicitarmesa" element={<RutaPrivada requiredRole="usuario"><SolicitarMesa /></RutaPrivada>} />
+          <Route path="/cargacorrecta" element={<RutaPrivada requiredRole="usuario"><CargaCorrecta /></RutaPrivada>} />
+          <Route path="/InscripcionMateria" element={<RutaPrivada requiredRole="usuario"><InscripcionMateria /></RutaPrivada>} />
+          <Route path="/InscriptoCorrectamente" element={<RutaPrivada requiredRole="usuario"><InscriptoCorrectamente /></RutaPrivada>} />
+          <Route path="/Contacto" element={<RutaPrivada requiredRole="usuario"><Contacto /></RutaPrivada>} />
+          <Route path="/Gracias" element={<RutaPrivada requiredRole="usuario"><Gracias /></RutaPrivada>} />
+
+          {/* Rutas para admin */}
+          <Route path="/mesa-examen" element={<RutaPrivada requiredRole="admin"><UseMesaExamen /></RutaPrivada>} />
+          <Route path="/alta-mesa" element={<RutaPrivada requiredRole="admin"><AltaMesaExamen /></RutaPrivada>} />
+          <Route path="/producto/:id" element={<RutaPrivada requiredRole="admin"><Detalle /></RutaPrivada>} />
+
+        </Routes>
+      </BrowserRouter>
     </UserProvider>
   );
 }
