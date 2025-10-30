42c2ff3cb1ca547faf216bbe10a3c7f6f33901a1
CristianBr2 <antinir.cristian.23@gmail.com>
2025-10-30 20:45:03 -0300

    Realizando tabla para los ṕroductos por ID

--- src/components/Detalle.jsx
+++ src/components/Detalle.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
 
 const productos = [
-    {id: 1, nombre: 'Matemáticas', descripcion: 'detalles'},
-    {id: 2, nombre: 'Programación', descripcion: 'detalles'},
-    {id: 3, nombre: 'Sistemas Operativo', descripcion: 'detalles'},
+    {id: 1, nombremateria: 'Matemáticas', profesor: 'Juan Castro', curso:'4° 2°', horario: '19:30 hs a 20:50 hs'},
+    {id: 2, nombremateria: 'Programación', profesor: 'Lucas Alvarado', curso: '5° 1°', horario: '18:00 hs a 19:20 hs'},
+    {id: 3, nombremateria: 'Sistemas Operativo', profesor: 'Erick Calderon', curso: '6° 1°', horario: '21:00 hs a 22:20 hs'},
+    {id: 4, nombremateria: 'Literatura', profesor: 'Emanuel Lagos', curso: '5° 1°', horario: '18:00 hs a 19:20 hs'},
+    {id: 5, nombremateria: 'Base de Datos', profesor: 'Teran Joaquin', curso: '5° 1°', horario: '18:00 hs a 19:20 hs'},
 ];      
 
 function Detalle() {
@@ -15,10 +17,26 @@ function Detalle() {
     }
 
     return(
-        <div>
-            <h1>{producto.nombre}</h1>
-            <p>{producto.descripcion}</p>
-        </div>
+        
+
+        <table className="DetalleTabla" border="1" style={{ margin: "0 auto", marginTop: "90px" }}>
+            <thead>
+                <tr>
+                    <th>Nombre Materia</th>
+                    <th>Profesor</th>
+                    <th>Curso</th>
+                    <th>Horario</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{producto.nombremateria}</td>
+                    <td>{producto.profesor}</td>
+                    <td>{producto.curso}</td>
+                    <td>{producto.horario}</td>
+                </tr>
+            </tbody>
+        </table> 
     );
 }
 
