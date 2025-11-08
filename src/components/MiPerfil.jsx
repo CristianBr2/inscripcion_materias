80259009d7f3a3b9eec214252bbedc8424cf71de
CristianBr2 <antinir.cristian.23@gmail.com>
2025-11-07 22:16:23 -0300

    Nuevo archivo 'MiPerfil.jsx'

---
+++ src/components/MiPerfil.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import './MiPerfil.css';
+
+function MiPerfil() {
+  const navigate = useNavigate();
+
+  return (
+    <>
+    
+    </>
+  );
+}
+
+export default MiPerfil;
